15c0dac05d8489ccc0e41f5dd2a9d7a3e4b42304
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-14 21:30:50 +0400

    Add button and function for redraw grid

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 let gridSize = 16;
 
-function drawGrid(gridSize) {
-  let container = document.createElement('div');
-  container.className = 'container';
-  container.setAttribute(
-    'style',
-    'display: flex; flex-wrap: wrap; width: 640px; height: 640px; margin: auto;'
-  );
+let button = document.createElement('button');
+button.textContent = 'Change grid size';
+document.body.appendChild(button);
+button.setAttribute(
+  'style',
+  'display: block; margin: auto; margin-bottom: 20px;'
+);
+button.addEventListener(
+  'click',
+  () => (gridSize = +prompt('Enter grid size', 16))
+);
+button.addEventListener('click', redrawGrid);
 
+let container = document.createElement('div');
+container.className = 'container';
+container.setAttribute(
+  'style',
+  'display: flex; flex-wrap: wrap; width: 640px; height: 640px; margin: auto;'
+);
+
+function drawGrid(gridSize) {
   for (let i = 0; i < gridSize ** 2; i++) {
     let cell = document.createElement('div');
     cell.className = 'cell';
@@ -19,20 +32,30 @@ function drawGrid(gridSize) {
   }
 
   document.body.appendChild(container);
+
+  container.addEventListener(
+    'mouseover',
+    (event) => {
+      // highlight the mouseenter target
+      event.target.style.background = 'purple';
+
+      setTimeout(() => {
+        event.target.style.background = 'lightgrey';
+      }, 500);
+    },
+    false
+  );
 }
 
 drawGrid(gridSize);
 
-let container = document.querySelector('.container');
-container.addEventListener(
-  'mouseover',
-  (event) => {
-    // highlight the mouseenter target
-    event.target.style.background = 'purple';
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
-    setTimeout(() => {
-      event.target.style.background = 'lightgrey';
-    }, 500);
-  },
-  false
-);
+function redrawGrid() {
+  removeAllChildNodes(container);
+  drawGrid(gridSize);
+}
